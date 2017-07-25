d625e96e261e0eb9918c2014bbd95c507b7ce91c
Stefan Huber <hubidu@users.noreply.github.com>
2017-07-25 12:49:25 +0200

    Make waitForTimeout configurable

--- driver.js
+++ driver.js
@@ -7,7 +7,10 @@ const { addExtensions } = require('./add-extensions')
 
 const configFile = path.join(process.cwd(), '_config.js')
 let config = {
-    caps: []
+    caps: [],
+    timeouts: {
+        waitForTimeout: 5000
+    }
 }
 if (fs.existsSync(configFile)) {
     config = require(configFile).default
@@ -32,6 +35,8 @@ module.exports = () => {
     const wdioInstance = new WebDriverIO({
                 url: 'not important but non empty',  // Not important set in test
                 browser: 'chrome', // TODO Should be configurable
+                smartWait: 5000, // TODO Try this
+                waitForTimeout: (config.timeouts && config.timeouts.waitForTimeout)? config.timeouts.waitForTimeout : 5000,
                 // windowSize: 'maximize', // TODO does not seem to work with chrome headless "automation error"
 
                  desiredCapabilities: config.caps.indexOf('chrome-headless') > -1 ? chromeHeadless : {}
